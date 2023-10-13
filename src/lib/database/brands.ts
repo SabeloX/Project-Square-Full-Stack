0bd1d5243e5796ed27fe144d7d42ce2af37adf98
SabeloX <sabelomtw@gmail.com>
2023-10-13 16:00:22 +0200

    Get brands from database

---
+++ src/lib/database/brands.ts
@@ -0,0 +1,29 @@
+import { Collection, Db } from "mongodb";
+import clientPromise from "./connect";
+
+let client;
+let db: Db | undefined;
+let brands: Collection;
+
+const init = async () => {
+    if (db) return;
+    try {
+        client = await clientPromise;
+        db = await client.db();
+        brands = db.collection('brands');
+    }
+    catch (error: any) {
+        throw new Error('Database connection failed.');
+    }
+}
+
+export const getBrands = async () => {
+    try {
+        if (!brands) await init();
+        const results = await brands.find({}).map(item => ({ ...item, _id: item._id.toString() }));
+        return { brands: results }
+    }
+    catch (error) {
+        return { error, message: "Failed to fetch brands." }
+    }
+}
--- src/lib/database/connect.ts
+++ src/lib/database/connect.ts
@@ -5,10 +5,12 @@ const uri = process.env.DATABASE_URL;
 if (!uri) throw new Error("Database not found!");
 
 let client = new MongoClient(uri);
-let clientPromise;
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV !== "production") {
-    if (!global._mongoClientPromise) global._mongoClientPromise = client.connect(); // use a connect that exists
+    if (!global._mongoClientPromise) global._mongoClientPromise = client.connect(); // use a connection that exists
     clientPromise = global._mongoClientPromise;
 }
 else clientPromise = client.connect();
+
+export default clientPromise;
